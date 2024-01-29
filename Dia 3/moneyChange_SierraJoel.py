5dedbbcd60a3732e74610a84ffb52b2fe8521427
Santiago <j.santiagolseon@gmail.com>
2024-01-29 09:16:15 -0500

    creacion del trabajo

---
+++ Dia 3/moneyChange_SierraJoel.py
@@ -0,0 +1,24 @@
+## Define coins as value
+coin1 = 1
+coin2 = 5
+coin3 = 10
+
+## Define Temporal value as temporal for the main function
+temporalValue = int
+
+substraction = int
+rest = int
+
+count = int(input())
+
+while count > 9:
+    for i in range(0,1,1):
+        substraction = count - coin3
+        temporalValue = rest+1
+
+while count > coin1:
+    for i in range(0,1,1):
+        
+
+
+
